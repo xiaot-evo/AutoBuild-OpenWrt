ebda406cbb8342780551ca4d70ad4978f523c7fb
xiaot-evo <3258412091@qq.com>
2026-08-05 03:21:54 +0800

    Restore global customize.sh, change default IP to 192.168.10.1
    - Restore global customize.sh run during Configuration step
    - Change default IP from 192.168.5.1 to 192.168.10.1
    - Remove duplicate IP change from device scripts

--- devices/JDCloud_AX3000/customize.sh
+++ devices/JDCloud_AX3000/customize.sh
@@ -4,5 +4,4 @@
 # Lisence: MIT
 #=================================================
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# Add device-specific customizations here
